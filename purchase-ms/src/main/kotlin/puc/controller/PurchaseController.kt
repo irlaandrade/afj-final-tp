905506a200c6d5ef0bb675df187556353a8fbd23
irlaandrade <paula.irla@gmail.com>
2024-09-01 15:33:24 -0300

    feat: remove code

--- purchase-ms/src/main/kotlin/puc/controller/PurchaseController.kt
+++ purchase-ms/src/main/kotlin/puc/controller/PurchaseController.kt
@@ -1,6 +1,5 @@
 package puc.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
@@ -22,7 +21,6 @@ class PurchaseController(val rabbitTemplate: RabbitTemplate, val jwtUtil: JwtUti
     @PostMapping("/buy")
     fun buy(@RequestHeader("Authorization") token: String, @RequestBody purchaseRequest: PurchaseRequest): ResponseEntity<String> {
         val bearer = token.replaceFirst("Bearer ".toRegex(), "")
-//        val bearer = token.replaceFirst("Bearer ", "")
 
         if (!jwtUtil.validateToken(bearer)) {
             return ResponseEntity.status(401).body("Failed to authenticate.")
@@ -39,16 +37,6 @@ class PurchaseController(val rabbitTemplate: RabbitTemplate, val jwtUtil: JwtUti
         }
         return ResponseEntity.ok("Purchase request sent.")
     }
-
-//    @PostMapping("/buy")
-//    fun buy(@RequestHeader("Authorization") token: String, @RequestBody purchaseRequest: PurchaseRequest): ResponseEntity<String> {
-//        val userId = jwtUtil.extractUserId(token.removePrefix("Bearer "))
-//        val purchaseMessage = PurchaseMessage(userId, purchaseRequest.productId, purchaseRequest.quantity)
-//        val objectMapper = ObjectMapper()
-//        val messageAsString = objectMapper.writeValueAsString(purchaseMessage)
-//        rabbitTemplate.convertAndSend(exchange, routingKey, messageAsString)
-//        return ResponseEntity.ok("Purchase request sent.")
-//    }
 }
 
 data class PurchaseRequest(val productId: String, val quantity: Int)
--- purchase-ms/src/main/kotlin/puc/util/JwtUtil.kt
+++ purchase-ms/src/main/kotlin/puc/util/JwtUtil.kt
@@ -2,80 +2,53 @@ package puc.util
 
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
-import java.util.*
-import javax.crypto.SecretKey
+import java.util.Date
+import java.util.UUID
 
 @Component
 class JwtUtil {
 
-//    private val logger = LoggerFactory.getLogger(JwtUtil::class.java)
-//    var secretString = "nkGzT7rFzRfPiudFmPbK_3YRneD1sLqeCM-h5N45UsM"
-//    private val secretString = UUID.randomUUID().toString()
-
-//    private val secretKey: SecretKey = Keys.hmacShaKeyFor(secretString.toByteArray(StandardCharsets.UTF_8))
-//    private val expirationTime = 1000 * 60 * 60 // 1 hora
-
-//    fun validateToken(token: String): Boolean {
-//        return try {
-//            val secretKey = Keys.hmacShaKeyFor(secretString.toByteArray(StandardCharsets.UTF_8))
-//            val claims = Jwts.parserBuilder()
-//                .setSigningKey(secretKey)
-//                .build()
-//                .parseClaimsJws(token)
-//                .body
-//
-//            if (claims.expiration.before(Date())) {
-//                logger.error("Token expirado")
-//                return false
-//            }
-//
-//            true
-//        } catch (e: Exception) {
-//            logger.error("Erro ao validar token: {}", e.message)
-//            false
-//        }
-//    }
-
-//    fun validateToken(token: String): Boolean {
-//        return try {
-//            val secretKey = Keys.hmacShaKeyFor(secretString.toByteArray(StandardCharsets.UTF_8))
-//            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token)
-//            true
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            false
-//        }
-//    }
-
-//    fun extractUserId(token: String): Long? {
-//        return try {
-//            val secretKey = Keys.hmacShaKeyFor(secretString.toByteArray(StandardCharsets.UTF_8))
-//            val claims: Claims = Jwts.parserBuilder()
-//                .setSigningKey(secretKey)
-//                .build()
-//                .parseClaimsJws(token)
-//                .body
-//
-//            (claims["userId"] as String).toLong()
-//        } catch (e: Exception) {
-//            logger.error("Erro ao extrair userId: {}", e.message)
-//            null
-//        }
-//    }
-
-
-    private val secretKey = "mySecretKey"
+    private val logger = LoggerFactory.getLogger(JwtUtil::class.java)
+    private val secretString = UUID.randomUUID().toString()
 
     fun validateToken(token: String): Boolean {
-        return true
+        return try {
+            val secretKey = Keys.hmacShaKeyFor(secretString.toByteArray(StandardCharsets.UTF_8))
+            val claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .body
+
+            if (claims.expiration.before(Date())) {
+                logger.error("Token expirado")
+                return false
+            }
+
+            true
+        } catch (e: Exception) {
+            logger.error("Erro ao validar token: {}", e.message)
+            false
+        }
     }
 
-    fun extractUserId(token: String): Long {
-        return 1
+    fun extractUserId(token: String): Long? {
+        return try {
+            val secretKey = Keys.hmacShaKeyFor(secretString.toByteArray(StandardCharsets.UTF_8))
+            val claims: Claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .body
+
+            (claims["userId"] as String).toLong()
+        } catch (e: Exception) {
+            logger.error("Erro ao extrair userId: {}", e.message)
+            null
+        }
     }
 }
